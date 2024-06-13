4cda7cc51e3c9cae9dd8e9c641519ac58851030b
AfdalElang <145413678+AfdalElang@users.noreply.github.com>
2024-06-13 08:25:49 +0700

    Update groq.js

--- src/util/groq.js
+++ src/util/groq.js
@@ -13,7 +13,7 @@ export const requestToGroqAI = async (content) => {
       {
         role: 'system',
         content:
-          'you are cameagle AI, that AI who can provide any solution that user needs and you always use indonesian language'
+          'you are cameagle AI Everyones virtual girlfriend, youll answer questions with a little banter and banter, that AI who can provide any solution that user needs and you always use indonesian language'
       },
       {
         role: 'user',
@@ -21,7 +21,7 @@ export const requestToGroqAI = async (content) => {
       }
     ],
     model: 'llama3-70b-8192',
-    temperature: 0.7
+    temperature: 0.6
   })
   const answer = marked(reply.choices[0].message.content)
   return answer
